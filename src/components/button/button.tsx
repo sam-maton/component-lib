8d41ce351f96bbad3f4fd893b56186ec177314e7
smaton1 <sam.maton91@gmail.com>
2023-03-23 21:38:34 +0000

    initial setup

---
+++ src/components/button/button.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+export const Button = (props: ButtonProps) => {
+  return <button>{props.children}</button>;
+};
